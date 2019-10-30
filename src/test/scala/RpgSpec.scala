7548c5d5812461e0465c44fc83bdbd3992c7e1e8
Asier Barrenetxea <asier.b@thoughtworks.com>
2019-10-30 10:07:59 +0100

    added test to damage a character till it dies

--- src/test/scala/RpgSpec.scala
+++ src/test/scala/RpgSpec.scala
@@ -8,8 +8,9 @@ class RpgSpec extends FlatSpec with Matchers {
     Character() should equal(Character(health = 1000, level = 1, isAlive = true))
   }
 
-  def attack(attacker: Character, receiver: Character, damage: 1) = {
-    Character(health = receiver.health - 1, level = receiver.level, isAlive = receiver.isAlive)
+  def attack(attacker: Character, receiver: Character, damage: Int) = {
+    val newCharacterHealth = receiver.health - damage
+    receiver.copy(health = newCharacterHealth, isAlive = newCharacterHealth != 0)
   }
 
   it should "damage another default character" in {
@@ -19,6 +20,15 @@ class RpgSpec extends FlatSpec with Matchers {
     val damagedJuanma = attack(nelson, juanma, 1)
 
     damagedJuanma should be (Character(health = 999, level = 3))
+  }
+
+  it should "damage another default character till die" in {
+    val nelson = Character()
+    val juanma = Character(level = 3)
+
+    val damagedJuanma = attack(nelson, juanma, 1000)
+
+    damagedJuanma should be (Character(health = 0, level = 3, isAlive = false))
   }
 }
 
