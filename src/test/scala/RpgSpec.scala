bf94c969344bdcc07235b8c6d069a5f0b0bd9ea0
Asier Barrenetxea <asier.b@thoughtworks.com>
2019-10-30 10:15:10 +0100

    Attack to character with health lower than health

--- src/test/scala/RpgSpec.scala
+++ src/test/scala/RpgSpec.scala
@@ -9,8 +9,8 @@ class RpgSpec extends FlatSpec with Matchers {
   }
 
   def attack(attacker: Character, receiver: Character, damage: Int) = {
-    val newCharacterHealth = receiver.health - damage
-    receiver.copy(health = newCharacterHealth, isAlive = newCharacterHealth != 0)
+    val healthAfterDamage = if (damage > receiver.health) 0 else receiver.health - damage
+    receiver.copy(health = healthAfterDamage, isAlive = healthAfterDamage != 0)
   }
 
   it should "damage another default character" in {
@@ -19,7 +19,7 @@ class RpgSpec extends FlatSpec with Matchers {
 
     val damagedJuanma = attack(nelson, juanma, 1)
 
-    damagedJuanma should be (Character(health = 999, level = 3))
+    damagedJuanma should be(Character(health = 999, level = 3))
   }
 
   it should "damage another default character till die" in {
@@ -28,7 +28,16 @@ class RpgSpec extends FlatSpec with Matchers {
 
     val damagedJuanma = attack(nelson, juanma, 1000)
 
-    damagedJuanma should be (Character(health = 0, level = 3, isAlive = false))
+    damagedJuanma should be(Character(health = 0, level = 3, isAlive = false))
+  }
+
+  it should "damage another character with health lower than damage" in {
+    val nelson = Character()
+    val juanma = Character(level = 3, health = 50)
+
+    val damagedJuanma = attack(nelson, juanma, 1000)
+
+    damagedJuanma should be(Character(health = 0, level = 3, isAlive = false))
   }
 }
 
