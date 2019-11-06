ccc1383c7610b37448a938a8dca946d93a77d55b
Asier Barrenetxea <asier.b@thoughtworks.com>
2019-11-06 09:41:20 +0100

    Characters can heal

--- src/test/scala/RpgSpec.scala
+++ src/test/scala/RpgSpec.scala
@@ -39,5 +39,51 @@ class RpgSpec extends FlatSpec with Matchers {
 
     damagedJuanma should be(Character(health = 0, level = 3, isAlive = false))
   }
+
+
+  def heal(from: Character, to: Character, health: Int): Character = {
+    to match {
+      case Character(x,_,_) if (x + health) > 1000 => to.copy(health = 1000)
+      case Character(_,_,true) => to.copy(health = to.health + health)
+      case _ => to
+    }
+  }
+
+  it should "juanma heals nelson" in {
+    val nelson = Character(health = 1)
+    val juanma = Character()
+    val healedNelson = heal(juanma, nelson, 99)
+    healedNelson should be(Character(health = 100))
+  }
+
+  it should "juanma doesnt heal dead nelson" in {
+    val deadNelson = Character(health = 0, isAlive = false)
+    val juanma = Character()
+    val healedNelson = heal(juanma, deadNelson, 99)
+    healedNelson should be(deadNelson)
+  }
+
+  it should "when juanma tries to heal a full health nelson, nelson health does not increase" in {
+    val fullytHealthNelson = Character(health = 1000)
+    val juanma = Character()
+    val healedNelson = heal(juanma, fullytHealthNelson, 1)
+    healedNelson should be(fullytHealthNelson)
+  }
+
+  it should "when juanma tries to heal a full health nelson, nelson health can not increase over 1000" in {
+    val nelson = Character(health = 999)
+    val juanma = Character()
+    val healedNelson = heal(juanma, nelson, 2)
+    val expectedNelson = Character(health = 1000)
+    healedNelson should be(expectedNelson)
+  }
+
+  it should "when juanma tries to heal a full health dead nelson, nelson health does not increase" in {
+    val fullytHealthNelson = Character(health = 1000, isAlive = false)
+    val juanma = Character()
+    val healedNelson = heal(juanma, fullytHealthNelson, 1)
+    healedNelson should be(fullytHealthNelson)
+  }
+
 }
 
