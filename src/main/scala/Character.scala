aad4e6a2023c617f3cd9a81fb27a96be6b5b1c0c
Nelson Jumbo <njunbo@gmail.com>
2019-11-22 17:10:10 +0100

    Refactor previous to iteration 3 :)

--- src/main/scala/Character.scala
+++ src/main/scala/Character.scala
@@ -4,8 +4,6 @@ case class Alive(health: Double) extends Health
 
 case class Dead() extends Health
 
-case class Character(health: Health = Health(1000), level: Int = 1)
-
 object Health {
   def apply(health: Double): Health = {
     if (health > 0) {
@@ -16,29 +14,47 @@ object Health {
   }
 }
 
+trait Character{
+  val health: Health
+  val level: Int
+}
+case class MeleeCharacter(health: Health = Health(1000), level: Int = 1) extends Character
+case class RangedCharacter(health: Health = Health(1000), level: Int = 1) extends Character
+
 object Character {
-  def attack(attacker: Character, receiver: Character, damage: Int) = {
+  val DamageThreshold = 5
+
+  def copyWithHealth(character: Character, newHealth: Health): Character = {
+    character match {
+      case x: MeleeCharacter => x.copy(health = newHealth)
+      case x: RangedCharacter => x.copy(health = newHealth)
+    }
+  }
+
+  def attack(attacker: Character, receiver: Character, damage: Int, distance: Int) = {
     if (attacker == receiver) {
       attacker
     } else {
       val newDamage = recalculateDamage(attacker, receiver, damage)
-      receiver.copy(health = recalculateHealth(receiver, newDamage, _ - _))
+      Character.copyWithHealth(receiver, recalculateHealth(receiver, newDamage, _ - _))
     }
   }
 
   private def recalculateDamage(attacker: Character, receiver: Character, damage: Int): Double = {
-    if (attacker.level - receiver.level >= 5) {
-      damage * 1.5
-    }else if (receiver.level - attacker.level >= 5) {
-      damage / 2
-    } else {
-      damage
+    val levelDiff = attacker.level - receiver.level
+
+    val greaterAttacker = levelDiff >= DamageThreshold
+    val lowerAttacker = levelDiff <= -DamageThreshold
+    (greaterAttacker, lowerAttacker) match  {
+      case (true, _) =>  damage * 1.5
+      case (_, true) =>  damage / 2
+      case _ => damage
     }
   }
 
   def heal(from: Character, to: Character, health: Int): Character = {
     if (from == to) {
-      to.copy(health = recalculateHealth(to, health, _ + _))
+      Character.copyWithHealth(to, recalculateHealth(to, health, _ + _))
     } else {
       to
     }
--- src/test/scala/RpgSpec.scala
+++ src/test/scala/RpgSpec.scala
@@ -4,107 +4,92 @@ import Character._
 class RpgSpec extends FlatSpec with Matchers {
 
   it should "create a character" in {
-    Character() should equal(Character(health = Alive(1000)))
+    MeleeCharacter() should equal(MeleeCharacter(health = Alive(1000)))
   }
 
   it should "damage another default character" in {
-    val nelson = Character()
-    val juanma = Character(level = 3)
+    val nelson = MeleeCharacter()
+    val juanma = MeleeCharacter(level = 3)
 
-    val damagedJuanma = attack(nelson, juanma, 1)
+    val damagedJuanma = attack(nelson, juanma, 1, 0)
 
-    damagedJuanma should be(Character(health = Alive(999), level = 3))
+    damagedJuanma should be(MeleeCharacter(health = Alive(999), level = 3))
   }
 
   it should "damage another default character till die" in {
-    val nelson = Character()
-    val juanma = Character(level = 3)
+    val nelson = MeleeCharacter()
+    val juanma = MeleeCharacter(level = 3)
 
-    val damagedJuanma = attack(nelson, juanma, 1000)
+    val damagedJuanma = attack(nelson, juanma, 1000, 0)
 
-    damagedJuanma should be(Character(health = Dead(), level = 3))
+    damagedJuanma should be(MeleeCharacter(health = Dead(), level = 3))
   }
 
   it should "damage another character with health lower than damage" in {
-    val nelson = Character()
-    val juanma = Character(level = 3, health = Alive(50))
+    val nelson = MeleeCharacter()
+    val juanma = MeleeCharacter(level = 3, health = Alive(50))
 
-    val damagedJuanma = attack(nelson, juanma, 1000)
+    val damagedJuanma = attack(nelson, juanma, 1000, 0)
 
-    damagedJuanma should be(Character(health = Dead(), level = 3))
+    damagedJuanma should be(MeleeCharacter(health = Dead(), level = 3))
   }
 
   it should "juanma tries to heal nelson, but fails" in {
-    val nelson = Character(health = Alive(1))
-    val juanma = Character()
+    val nelson = MeleeCharacter(health = Alive(1))
+    val juanma = MeleeCharacter()
     val healedNelson = heal(juanma, nelson, 99)
     healedNelson should be(nelson)
   }
 
   it should "juanma doesnt heal dead juanma" in {
-    val deadJuanma = Character(health = Dead())
+    val deadJuanma = MeleeCharacter(health = Dead())
     val healedJuanma = heal(deadJuanma, deadJuanma, 99)
     healedJuanma should be(deadJuanma)
   }
 
   it should "when juanma tries to heal a full health juanma, juanma's health does not increase" in {
-    val fullytHealthJuanma = Character(health = Alive(1000))
+    val fullytHealthJuanma = MeleeCharacter(health = Alive(1000))
     val healedJuanma = heal(fullytHealthJuanma, fullytHealthJuanma, 1)
     healedJuanma should be(fullytHealthJuanma)
   }
 
   it should "when juanma tries to heal a full health juanma, juanma's health can not increase over 1000" in {
-    val juanma = Character(health = Alive(999))
+    val juanma = MeleeCharacter(health = Alive(999))
     val healedJuanma = heal(juanma, juanma, 2)
-    val expectedJuanma = Character(health = Alive(1000))
+    val expectedJuanma = MeleeCharacter(health = Alive(1000))
     healedJuanma should be(expectedJuanma)
   }
 
   it should "when juanma tries to damage himself, nothing happens" in {
-    val juanma = Character()
-    val damagedJuanma = attack(juanma, juanma, 2)
+    val juanma = MeleeCharacter()
+    val damagedJuanma = attack(juanma, juanma, 2, 0)
     damagedJuanma should be(juanma)
   }
 
   it should "when juanma tries to heal himself, its health increases" in {
-    val juanma = Character(Alive(700))
+    val juanma = MeleeCharacter(Alive(700))
     val healedJuanma = heal(juanma, juanma, 10)
-    healedJuanma should be(Character(Alive(710)))
+    healedJuanma should be(MeleeCharacter(Alive(710)))
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   it should "If the target is 5 Levels above the attacker, Damage is reduced by 50%" in {
-    val javier = Character(level = 1)
-    val enrique = Character(Alive(700),level = 6)
-    val attackedEnrique = attack(javier, enrique, 10)
-    attackedEnrique should be(Character(Alive(695), level = 6))
+    val javier = MeleeCharacter(level = 1)
+    val enrique = MeleeCharacter(Alive(700),level = 6)
+    val attackedEnrique = attack(javier, enrique, 10, 0)
+    attackedEnrique should be(MeleeCharacter(Alive(695), level = 6))
   }
 
   it should "If the target is more than 5 Levels above the attacker, Damage is reduced by 50%" in {
-    val javier = Character(level = 1)
-    val enrique = Character(Alive(700),level = 27)
-    val attackedEnrique = attack(javier, enrique, 10)
-    attackedEnrique should be(Character(Alive(695), level = 27))
+    val javier = MeleeCharacter(level = 1)
+    val enrique = MeleeCharacter(Alive(700),level = 27)
+    val attackedEnrique = attack(javier, enrique, 10, 0)
+    attackedEnrique should be(MeleeCharacter(Alive(695), level = 27))
   }
 
   it should "If the target is 5 levels or below the attacker, Damage is increased by 50%" in {
-    val javier = Character(Alive(1000),level = 1)
-    val enrique = Character(level = 27)
-    val attackedJavier = attack(enrique, javier, 10)
-    attackedJavier should be(Character(Alive(985), level = 1))
+    val javier = MeleeCharacter(Alive(1000),level = 1)
+    val enrique = MeleeCharacter(level = 27)
+    val attackedJavier = attack(enrique, javier, 10, 0)
+    attackedJavier should be(MeleeCharacter(Alive(985), level = 1))
   }
 }
